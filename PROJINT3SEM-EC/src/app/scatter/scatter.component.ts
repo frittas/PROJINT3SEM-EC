3b53905419d46a2aeb74cb8ba3c5cbfba96149ed
Rafael Freitas <faelfreitas00@gmail.com>
2023-03-02 23:42:51 +0000

    adding chart components

---
+++ PROJINT3SEM-EC/src/app/scatter/scatter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scatter',
+  templateUrl: './scatter.component.html',
+  styleUrls: ['./scatter.component.scss']
+})
+export class ScatterComponent {
+
+}
